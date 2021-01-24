fea6e5b81f3af160e142993cc1e92b85eba5733b
Grosty <grostmaster@gmail.com>
2021-01-24 12:19:22 +0200

    init project

---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Test component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
